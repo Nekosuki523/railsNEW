dd4176a112f102b27c5442ce8efdcb11e421d6bd
Nekosuki523 <sakyu523ms_xzxz@yahoo.co.jp>
2023-06-25 16:47:28 +0000

    dbのメソッドエラーの修正

--- db/migrate/20230614094450_create_users.rb
+++ db/migrate/20230614094450_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :title
-      dt.date :start_date
+      t.date :start_date
       t.date :end_date
       t.string :allday
       tboolean :checkbox

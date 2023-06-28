d4e674c553176b885ede665a49825975ca74cde1
Nekosuki523 <sakyu523ms_xzxz@yahoo.co.jp>
2023-06-28 02:58:46 +0000

    新規予定で全て空欄の場合のバリデーション実装追加とコメント頂いた部分の修正

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -6,9 +6,9 @@ class Todo < ApplicationRecord
    
    validate :start_end_check
    
-   def start_end_check
-      errors.add(:end_date, "は開始日より前の日付は登録できません。") unless
-      self.start_date <= self.end_date
-   end 
-   
+  def start_end_check
+  if start_date.present? && self.start_date <= self.end_date
+   errors.add(:end_date, "は開始日より前の日付は登録できません。") 
+  end 
+  end
 end

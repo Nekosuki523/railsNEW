90a28a0f56daccb1e693a487592ecf941cf4dd9a
Nekosuki523 <sakyu523ms_xzxz@yahoo.co.jp>
2023-06-28 16:02:32 +0000

    日付逆転できないよう修正

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -7,8 +7,10 @@ class Todo < ApplicationRecord
    validate :start_end_check
    
   def start_end_check
-  if start_date.present? && self.start_date <= self.end_date
-   errors.add(:end_date, "は開始日より前の日付は登録できません。") 
+  if start_date.nil?
+    start_date
+  elsif self.start_date > self.end_date
+    errors.add(:end_date, "は開始日より前の日付は登録できません。") 
   end 
   end
 end
